725395c825eec86301c4072993efeedc3f42759b
Xiongxin Xiao <Xiongxin.Xiao@ibm.com>
2022-02-18 11:31:26 -0500

    last commit before refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CoffeesResolver } from './coffee/coffee.resolve';
-import { CoffeeComponent } from './coffee/coffee/coffee.component';
 
 const routes: Routes = [
   {
